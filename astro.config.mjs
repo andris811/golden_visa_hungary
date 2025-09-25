9d7cb8c895827ed8e5b1e7a0f5835701fa21173e
Andras Varga <vargan89@gmail.com>
2025-09-25 17:25:23 +0800

    fix: base path, root redirect, base-aware links

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://andris811.github.io/golden_visa_hungary', // update later if custom domain
+  base: '/golden_visa_hungary',
+  // trailingSlash: 'always',
+  integrations: [sitemap()]
+});
